09ef46b5f34701d04d53186837c5669f4f68e7d8
AustenVanBrogen <austenvanbrogen@yahoo.com>
2022-11-02 17:17:40 -0500

    Added components folder as well as wrapper, screen, buttonBox, and Button components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-
+import React, {useRef} from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <div>
+        <button>C</button>
+        <button>()</button>
+        <button>N</button>
+        <button>÷</button>
+      </div>
+      <div>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>X</button>
+      </div>
+      <div>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+      </div>
+      <div>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>+</button>
+      </div>
+      <div>
+        <button>0</button>
+        <button>.</button>
+        <button>B</button>
+        <button>=</button>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Wrapper.js
@@ -0,0 +1,7 @@
+import './Wrapper.css';
+
+const Wrapper = ({children}) =>{
+    return <div classname='wrapper'>{children}</div>
+}
+
+export default Wrapper;
